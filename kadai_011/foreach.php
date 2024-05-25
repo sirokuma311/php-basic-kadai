03254ab992b9a9d1686bd36ff0a76acbda2ee92f
sirokuma311 <167377216+sirokuma311@users.noreply.github.com>
2024-05-25 17:23:03 +0900

    Add files via upload

---
+++ kadai_011/foreach.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+            $vegetables = ['名前' => '玉ねぎ', '値段' => 200, '産地' => '北海道', ];
+            foreach ($vegetables as $key => $value){
+                echo "{$key} : {$value}<br>";
+            }
+        ?>
+    </p>
+</body>
+</html>
