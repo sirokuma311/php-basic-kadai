bb7592b9e2d40349a77be2a96fb5b2687d842670
sirokuma311 <167377216+sirokuma311@users.noreply.github.com>
2024-06-01 15:43:00 +0900

    Add files via upload

---
+++ kadai_web_app/complete.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>登録が完了しました。</h1>
+    <p>社員情報がデータベースに保存されました。</p>
+    <p>
+        <button id="back-btn" onclick="location.href='form.php'">戻る</button>
+    </p>
+</body>
+</html>
